b34db8a22ce1e7a8527ec833dc9482fcc00d7d2b
AndrewMarron <amarron314@gmail.com>
2020-10-14 15:05:00 -0400

    Tweaks to models

--- Models/Models/Position.cs
+++ Models/Models/Position.cs
@@ -9,13 +9,13 @@ namespace Models.Models
     {
         private int positionId;
         private int projectId;
-        private string contractorId;
+        private int contractorId;
         private string positionTitle;
         private string description;
         private Project thisProject;
         private Contractor thisContractor;
 
-        public Position(int projectId, string contractorId, string positionTitle, string description)
+        public Position(int projectId, int contractorId, string positionTitle, string description)
         {
             ProjectId = projectId;
             ContractorId = contractorId;
@@ -25,7 +25,7 @@ namespace Models.Models
 
         public int PositionId { get => positionId; set => positionId = value; }
         public int ProjectId { get => projectId; set => projectId = value; }
-        public string ContractorId { get => contractorId; set => contractorId = value; }
+        public int ContractorId { get => contractorId; set => contractorId = value; }
         public string PositionTitle { get => positionTitle; set => positionTitle = value; }
         public string Description { get => description; set => description = value; }
         internal Project ThisProject { get => thisProject; set => thisProject = value; }
--- Models/Models/Project.cs
+++ Models/Models/Project.cs
@@ -14,15 +14,17 @@ namespace Models.Models
         private DateTime endDate;
         private double paymentOffered;
         private string projectName;
+        private string description;
         private Client thisClient;
 
-        public Project(int clientId, DateTime startDate, DateTime endDate, double paymentOffered, string projectName)
+        public Project(int clientId, DateTime startDate, DateTime endDate, double paymentOffered, string projectName, string description)
         {
             ClientId = clientId;
             StartDate = startDate;
             EndDate = endDate;
             PaymentOffered = paymentOffered;
             ProjectName = projectName;
+            Description = description;
         }
 
         public int ProjectId { get => projectId; set => projectId = value; }
@@ -31,6 +33,7 @@ namespace Models.Models
         public DateTime EndDate { get => endDate; set => endDate = value; }
         public double PaymentOffered { get => paymentOffered; set => paymentOffered = value; }
         public string ProjectName { get => projectName; set => projectName = value; }
+        public string Description { get => description; set => description = value; }
         internal Client ThisClient { get => thisClient; set => thisClient = value; }
     }
 }
