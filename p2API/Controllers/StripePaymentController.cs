a96d50c8ed6cc1959c7bf50fd6e83d4af21ee058
Zhongheng Sun <sunzh95@gmail.com>
2020-10-21 16:05:00 -0400

    stripe

--- p2API/Controllers/StripePaymentController.cs
+++ p2API/Controllers/StripePaymentController.cs
@@ -11,25 +11,34 @@ namespace p2API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StripePaymentController : ControllerBase
+    public class StripePaymentController : Controller
     {
 
         [HttpPost]
-        public PaymentIntent CreateCharge()
+        public IActionResult Post([FromBody] StripePaymentRequest paymentRequest)
         {
-            var options = new PaymentIntentCreateOptions
-            {
-                Amount = 10,
-                Currency = "usd",
-                PaymentMethodTypes = new List<string> {
-                                "card",
-                 },
-            };
+            StripeConfiguration.ApiKey = "pk_test_51He61ZCIJStw0TfUavdgaagOU0HkIFp8fcrM1BzvVu6o8OtwXlIsyin6l62zLDPbpkNLJMPbECs9x7TXRwV3OW1T00rs3UY7CT"
 
-            var service = new PaymentIntentService();
-            var intent = service.Create(options);
+            var myCharge = new ChargeCreateOptions();
+            myCharge.Source = paymentRequest.tokenId;
+            myCharge.Amount = paymentRequest.amount;
+            myCharge.Currency = "usd";
+            myCharge.Description = paymentRequest.productName;
+            myCharge.Metadata = new Dictionary<string, string>();
+            myCharge.Metadata["OurRef"] = "OurRef-" + Guid.NewGuid().ToString();
 
-            return intent;
+            var chargeService = new ChargeService();
+            var stripeCharge = chargeService.Create(myCharge);
+
+            return Json(stripeCharge);
+        }
+
+        public class StripePaymentRequest
+        {
+            public string tokenId { get; set; }
+            public string productName { get; set; }
+            public int amount { get; set; }
+            
         }
     }
 }
